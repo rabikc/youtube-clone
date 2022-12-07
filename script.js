6904d44baa6d8f420cc219a8dacecde2c60aeb5e
Rabi KC <rabikc40@gmail.com>
2022-12-08 01:11:24 +0545

    added mobile sidebar and toggle code

---
+++ script.js
@@ -0,0 +1,18 @@
+const toggleSidebarBtn = document.querySelector(".menu-btn");
+const mobSidebar = document.querySelector(".mob-sidebar");
+const mainSidebar = document.querySelector(".page-sidebar");
+const browseSection = document.querySelector(".browse-section");
+
+const menuToggle = () => {
+  if (window.getComputedStyle(mobSidebar).display === "none") {
+    mobSidebar.style.display = "block";
+    browseSection.style.maxWidth = "calc(100vw - 72px)";
+    browseSection.style.marginLeft = "72px";
+    mainSidebar.style.display = "none";
+  } else if (mobSidebar.style.display !== "none") {
+    mobSidebar.style.display = "none";
+    mainSidebar.style.display = "block";
+    browseSection.style.maxWidth = "calc(100vw - 240px)";
+    browseSection.style.marginLeft = "240px";
+  }
+};
