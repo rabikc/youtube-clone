c98515bf8ac7cd58187ef2dfd9438b6e9e7e8339
Rabi KC <rabikc40@gmail.com>
2022-12-08 17:10:55 +0545

    fixed some things

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const browseSection = document.querySelector(".browse-section");
 const menuToggle = () => {
   if (window.getComputedStyle(mobSidebar).display === "none") {
     mobSidebar.style.display = "block";
-    browseSection.style.maxWidth = "calc(100vw - 72px)";
+    browseSection.style.maxWidth = "calc(100vw - 70px)";
     browseSection.style.marginLeft = "72px";
     mainSidebar.style.display = "none";
   } else if (mobSidebar.style.display !== "none") {
@@ -17,5 +17,43 @@ const menuToggle = () => {
   }
 };
 
+// var x = window.matchMedia("(max-width: 1300px)");
+
+// function sidebarQuery(x) {
+//   if (x.matches) {
+//     mobSidebar.style.display = "block";
+//     mainSidebar.style.display = "none";
+//     browseSection.style.maxWidth = "calc(100vw - 72px)";
+//     browseSection.style.marginLeft = "72px";
+//   }
+// }
+
 var shortsContainer = document.querySelector(".shorts-section .container");
 console.log(shortsContainer.offsetWidth);
+
+const nextBtn = document.querySelector(".next-btn");
+const categoryChips = document.querySelectorAll(".single-chip");
+const chipsContainer = document.querySelector(".chips-container");
+
+console.log(categoryChips[0]);
+
+for (let i = 0; i < 17; i++) {
+  console.log(categoryChips[i]);
+}
+
+// const nextClick = () => {
+//   for (let i = 0; i < 17; i++) {
+//     chipsContainer.style.marginLeft =
+//   }
+// };
+
+const shortsSeeMoreBtn = document.querySelector(".see-more-btn");
+const shortsContainer = document.querySelector(
+  ".shorts-section .grid-container"
+);
+
+console.log(shortsContainer);
+
+const shortSeeMore = () => {
+  shortsContainer.style.minHeight = "100vh";
+};
